12218d941217dabbdc37a63b57093c37d1846abe
dsowles <d_sowles@mail.fhsu.edu>
2026-03-31 20:29:13 -0500

    Fixed quote router table name conflicts.

--- models/quote.php
+++ models/quote.php
@@ -26,9 +26,15 @@ class Quote {
         $this->conn = $db;
     }
 
-    // GET Quotes (Handles All, Author_id, Category_id, or Both)
+    // GET Quotes
     public function read($author_id = null, $category_id = null) {
+        /*
         $query = 'SELECT q.id, q.quote, a.author as author_name, c.category as category_name 
+                  FROM ' . $this->table . ' q
+                  LEFT JOIN authors a ON q.author_id = a.id
+                  LEFT JOIN categories c ON q.category_id = c.id';
+        */
+        $query = 'SELECT q.id, q.quote, a.author, c.category 
                   FROM ' . $this->table . ' q
                   LEFT JOIN authors a ON q.author_id = a.id
                   LEFT JOIN categories c ON q.category_id = c.id';
@@ -57,23 +63,41 @@ class Quote {
 
     // GET Single Quote
     public function read_single() {
+        /*
         $query = 'SELECT q.id, q.quote, a.author as author_name, c.category as category_name 
                   FROM ' . $this->table . ' q
                   LEFT JOIN authors a ON q.author_id = a.id
                   LEFT JOIN categories c ON q.category_id = c.id
                   WHERE q.id = ? LIMIT 1';
+        */
+        $query = 'SELECT q.id, q.quote, a.author, c.category 
+                  FROM ' . $this->table . ' q
+                  LEFT JOIN authors a ON q.author_id = a.id
+                  LEFT JOIN categories c ON q.category_id = c.id
+                  WHERE q.id = ? LIMIT 1';
+
 
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(1, $this->id);
         $stmt->execute();
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
+        /*
         if($row) {
             $this->quote = $row['quote'];
             $this->author_name = $row['author_name'];
             $this->category_name = $row['category_name'];
             return true;
         }
+        */
+
+        if($row) {
+            $this->quote = $row['quote'];
+            $this->author_name = $row['author'];
+            $this->category_name = $row['category'];
+            return true;
+        }
+
         return false;
     }
 
