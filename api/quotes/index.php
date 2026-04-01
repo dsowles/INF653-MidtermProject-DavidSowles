18fa75bfb36a9a15140f165260f8326dad29322c
dsowles <d_sowles@mail.fhsu.edu>
2026-03-31 20:13:05 -0500

    The quotes router logic did not have the CORS setup stuff.

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -16,12 +16,14 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 $method = $_SERVER['REQUEST_METHOD'];
 
-// Imports
-//include_once '../../config/Database.php';
-//include_once '../../models/Quote.php';
-//include_once '../../models/Author.php';
-//include_once '../../models/Category.php';
+// CORS stuff.
+if ($method === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+    exit();
+}
 
+// Imports
 include_once __DIR__ . '/../../config/database.php';
 include_once __DIR__ . '/../../models/quote.php'; 
 include_once __DIR__ . '/../../models/author.php';
